4e2649e327e953fd7b8e14ecac545ba766b64122
NyanLinHtet1999 <andydufresne323@gmail.com>
2024-01-07 18:16:35 +0630

    add vercel

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,4 @@
 const errorHandler =(err, req, res, next) => {
-     return res.status(500).json({msg : err})
+     return res.status(500).json({msg : `Something went wrong`})
 }
 module.exports = errorHandler
